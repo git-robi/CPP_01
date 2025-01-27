5d924966ffedad11c05f06d25352f952755949e1
Roberta Giambona <nemo@Robertas-MacBook-Pro.local>
2025-01-27 15:20:05 +0100

    refactor

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,18 +1,54 @@
+
 #include <iostream>
-#include "FileReplacer.hpp"
+#include <fstream>
+#include <string>
 
-int main(int argc, char **argv)
-{
+void replaceStrings(const std::string& inputFile, const std::string& s1, const std::string& s2) {
+    // Open the input file
+    std::ifstream inFile(inputFile);
+    if (!inFile) {
+        std::cerr << "Error: Cannot open file " << inputFile << std::endl;
+        return;
+    }
+
+    // Open the output file
+    std::ofstream outFile(inputFile + ".replace");
+    if (!outFile) {
+        std::cerr << "Error: Cannot create output file " << inputFile << ".replace" << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(inFile, line)) {
+        size_t pos = 0;
+        // Replace occurrences of s1 with s2 in the line
+        while ((pos = line.find(s1, pos)) != std::string::npos) {
+            line.erase(pos, s1.length());
+            line.insert(pos, s2);
+            pos += s2.length();
+        }
+        outFile << line << std::endl; // Write the modified line to the output file
+    }
+
+    inFile.close();
+    outFile.close();
+}
+
+int main(int argc, char* argv[]) {
     if (argc != 4) {
-        std::cerr << "Error: Program requires exactly 3 arguments" << std::endl;
-        std::cerr << "Usage: " << argv[0] << " <filename> <string1> <string2>" << std::endl;
+        std::cerr << "Wrong number of arguments." << std::endl;
         return 1;
     }
 
-    FileReplacer replacer(argv[1], argv[2], argv[3]);
-    if (!replacer.replaceContent()) {
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    if (s1.empty() || s2.empty()) {
+        std::cerr << "Error: Strings cannot be empty." << std::endl;
         return 1;
     }
 
+    replaceStrings(filename, s1, s2);
     return 0;
 }
