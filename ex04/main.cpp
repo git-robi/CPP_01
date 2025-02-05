0137eb6dc61d2b90e3f9622274d7eae7bac66103
Roberta Giambona <rgiambon@car14s6.42barcelona.com>
2025-02-05 11:47:28 +0100

    fix: removed cpp98 forbidden functions

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,48 +6,51 @@
 /*   By: rgiambon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 13:24:42 by rgiambon          #+#    #+#             */
-/*   Updated: 2025/02/03 13:25:20 by rgiambon         ###   ########.fr       */
+/*   Updated: 2025/02/05 11:46:28 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <iostream>
 #include <fstream>
 #include <string>
 
-void replaceStrings(const std::string& inputFile, const std::string& s1, const std::string& s2) {
-    // Open the input file
-    std::ifstream inFile(inputFile);
-    if (!inFile) {
+void replaceStrings(const std::string& inputFile, const std::string& s1, const std::string& s2)
+{
+    std::ifstream inFile(inputFile.c_str());
+    if (!inFile)
+	{
         std::cerr << "Error: Cannot open file " << inputFile << std::endl;
         return;
     }
 
-    // Open the output file
-    std::ofstream outFile(inputFile + ".replace");
-    if (!outFile) {
+    std::ofstream outFile((inputFile + ".replace").c_str());
+    if (!outFile)
+	{
         std::cerr << "Error: Cannot create output file " << inputFile << ".replace" << std::endl;
         return;
     }
 
     std::string line;
-    while (std::getline(inFile, line)) {
+    while (std::getline(inFile, line))
+	{
         size_t pos = 0;
-        // Replace occurrences of s1 with s2 in the line
-        while ((pos = line.find(s1, pos)) != std::string::npos) {
+        while ((pos = line.find(s1, pos)) != std::string::npos)
+		{
             line.erase(pos, s1.length());
             line.insert(pos, s2);
             pos += s2.length();
         }
-        outFile << line << std::endl; // Write the modified line to the output file
+        outFile << line << std::endl;
     }
 
     inFile.close();
     outFile.close();
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
+int main(int argc, char* argv[])
+{
+    if (argc != 4)
+	{
         std::cerr << "Wrong number of arguments." << std::endl;
         return 1;
     }
@@ -56,11 +59,14 @@ int main(int argc, char* argv[]) {
     std::string s1 = argv[2];
     std::string s2 = argv[3];
 
-    if (s1.empty() || s2.empty()) {
+    if (s1.empty() || s2.empty())
+	{
         std::cerr << "Error: Strings cannot be empty." << std::endl;
         return 1;
     }
 
     replaceStrings(filename, s1, s2);
-    return 0;
+    
+	return 0;
 }
+
